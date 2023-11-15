0e978ecdcccbf08c204012d3d88d29cc8e2ff3c6
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-15 09:12:39 +0100

    update the vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: 'https://github.com/fmanimashaun/dragNDrop', // This line is required for the app to work on GitHub Pages.
+  base: './',
   plugins: [],
   server: {
     host: '0.0.0.0',
-    hmr: true, // Change this line to false disable auto-refreshing.
-  },
-  build: {
-    assetsDir: 'assets', // The directory to nest generated assets under the outDir.
-    rollupOptions: {
-      output: {
-        manualChunks: undefined // Disable code splitting for simplicity.
-      }
-    }
+    hmr: true,
   }
 })
