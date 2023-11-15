a5650de53b5fcbb284c10bd2216593e2d778f515
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-15 08:47:02 +0100

    vite configuration update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
   server: {
     host: '0.0.0.0',
     hmr: true, // Change this line to false disable auto-refreshing.
+  },
+  build: {
+    assetsDir: 'assets', // The directory to nest generated assets under the outDir.
+    rollupOptions: {
+      output: {
+        manualChunks: undefined // Disable code splitting for simplicity.
+      }
+    }
   }
 })
